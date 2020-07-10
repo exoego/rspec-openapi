9af1235484e9a490a46f739f7471d9eea1826f76
Takashi Kokubun <takashikkbn@gmail.com>
2020-07-09 21:41:49 -0700

    Remove a redundant block

--- spec/rspec/openapi/rails_spec.rb
+++ spec/rspec/openapi/rails_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 require 'open3'
 
-RSpec.describe RSpec::OpenAPI do
+RSpec.describe 'rails request spec' do
   include SpecHelper
 
-  describe 'rails request spec' do
-    let(:openapi_path) { File.expand_path('spec/rails/doc/openapi.yaml', repo_root) }
+  let(:openapi_path) do
+    File.expand_path('spec/rails/doc/openapi.yaml', repo_root)
+  end
 
-    it 'generates the same spec/railsapp/doc/openapi.yaml' do
-      FileUtils.rm_f(openapi_path)
-      rspec 'spec/requests/rails', openapi: true
-      assert_run 'git', 'diff', '--exit-code', '--', openapi_path
-    end
+  it 'generates the same spec/railsapp/doc/openapi.yaml' do
+    FileUtils.rm_f(openapi_path)
+    rspec 'spec/requests/rails', openapi: true
+    assert_run 'git', 'diff', '--exit-code', '--', openapi_path
   end
 end
