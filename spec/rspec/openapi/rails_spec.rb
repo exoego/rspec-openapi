c880a9417d8511eb3d5eb47bff51603a8b26817f
Takashi Kokubun <takashikkbn@gmail.com>
2020-07-09 21:36:18 -0700

    Carve out SpecHelper

--- spec/rspec/openapi/rails_spec.rb
+++ spec/rspec/openapi/rails_spec.rb
@@ -2,23 +2,7 @@ require 'spec_helper'
 require 'open3'
 
 RSpec.describe RSpec::OpenAPI do
-  def repo_root
-    File.expand_path('../../..', __dir__)
-  end
-
-  def assert_run(*args)
-    out, err, status = Open3.capture3(*args)
-    expect(status.success?).to eq(true), "stdout:\n#{out}\nstderr:\n#{err}"
-  end
-
-  def rspec(*args, openapi: false)
-    env = { 'OPENAPI' => ('1' if openapi) }.compact
-    Bundler.with_unbundled_env do
-      Dir.chdir(repo_root) do
-        assert_run env, 'bundle', 'exec', 'rspec', '--pattern', 'spec/requests/**/*_spec.rb', *args
-      end
-    end
-  end
+  include SpecHelper
 
   describe 'rails request spec' do
     let(:openapi_path) { File.expand_path('spec/rails/doc/openapi.yaml', repo_root) }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,23 @@
+module SpecHelper
+  def repo_root
+    File.expand_path('..', __dir__)
+  end
+
+  def assert_run(*args)
+    out, err, status = Open3.capture3(*args)
+    expect(status.success?).to eq(true), "stdout:\n#{out}\nstderr:\n#{err}"
+  end
+
+  def rspec(*args, openapi: false)
+    env = { 'OPENAPI' => ('1' if openapi) }.compact
+    Bundler.with_unbundled_env do
+      Dir.chdir(repo_root) do
+        assert_run env, 'bundle', 'exec', 'rspec', '--pattern', 'spec/requests/**/*_spec.rb', *args
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
