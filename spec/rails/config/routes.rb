4e3d9789a6116aa3df06e6ec66e6c9c743534943
Mattia Roccoberton <mattia.roccoberton@treatwell.com>
2022-03-18 22:43:20 +0100

    Add a test with a block route

--- spec/rails/config/routes.rb
+++ spec/rails/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   defaults format: 'json' do
     resources :tables, only: [:index, :show, :create, :update, :destroy]
     resources :images, only: [:show]
+
+    get '/test_block' => ->(_env) { [200, { 'Content-Type' => 'text/plain' }, ['A TEST']] }
   end
 end
