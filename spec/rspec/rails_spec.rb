61bb1a0dea5d1c8c2a105c2f4fdd3e98609b5412
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2022-05-26 16:15:48 +0900

    Moved to rails-based testing

--- spec/rspec/rails_spec.rb
+++ spec/rspec/rails_spec.rb
@@ -31,4 +31,26 @@ RSpec.describe 'rails request spec' do
       expect(new_yaml).to eq org_yaml
     end
   end
+
+  describe "smart merge" do
+    let(:openapi_path_for_smart_merge) do
+      File.expand_path('spec/rails/doc/smart/openapi.json', repo_root)
+    end
+
+    let(:expected_path_for_smart_merge) do
+      File.expand_path('spec/rails/doc/smart/expected.json', repo_root)
+    end
+
+    it 'updates the spec/rails/doc/smart/openapi.json as same as in expected.json' do
+      original_source = File.read(openapi_path_for_smart_merge)
+      begin
+        rspec 'spec/requests/rails_smart_merge_spec.rb', openapi: true, output: :json
+        new_json = YAML.load(File.read(openapi_path_for_smart_merge))
+        expected_json = YAML.load(File.read(expected_path_for_smart_merge))
+        expect(new_json).to eq expected_json
+      ensure
+        File.write(openapi_path_for_smart_merge, original_source)
+      end
+    end
+  end
 end
