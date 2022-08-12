8551e3e8a55164b28d7cfa1eeabe0b8e6062969a
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2022-08-12 13:13:50 +0900

    Support references in component schemas

--- lib/rspec/openapi/components_updater.rb
+++ lib/rspec/openapi/components_updater.rb
@@ -10,6 +10,36 @@ class << RSpec::OpenAPI::ComponentsUpdater = Object.new
     top_level_refs = paths_to_top_level_refs(base)
     fresh_schemas = build_fresh_schemas(top_level_refs, base, fresh)
 
+    # Nested schema: References in Top-level schemas. May contain some top-level schema.
+    nested_refs = RSpec::OpenAPI::HashHelper::matched_paths(base, 'components.schemas.*.properties.*.$ref')
+
+    # We assume that super-deeply nested references are not common.
+    # Loop counter may exhaust if some of referenced are not generated due to removal. No need to raise error.
+    5.times.each do
+      generated_schema_names = fresh_schemas.keys
+      nested_refs = filter_non_generated_refs(nested_refs, base, generated_schema_names)
+
+      # Complete if all the referenced schemas are generated.
+      break if nested_refs.empty?
+
+      nested_refs.each do |paths|
+        parent_name = paths[-4]
+
+        # Skip if parent schema is not generated yet. It may be generated on next iteration.
+        next if fresh_schemas.dig(parent_name).nil?
+
+        property_name = paths[-2]
+        sub_schema = fresh_schemas.dig(parent_name, 'properties', property_name)
+
+        # Skip if the property using $ref is not found in the parent schema. The property may be removed.
+        next if sub_schema.nil?
+
+        schema_name = base.dig(*paths)&.gsub('#/components/schemas/', '')
+        fresh_schemas[schema_name] ||= {}
+        RSpec::OpenAPI::SchemaMerger.merge!(fresh_schemas[schema_name], sub_schema)
+      end
+    end
+
     # Clear out all properties before merge.
     # The properties using $ref are preserved since those are hand-crafted by user.
     fresh_schemas.keys.each do |schema_name|
@@ -50,6 +80,15 @@ class << RSpec::OpenAPI::ComponentsUpdater = Object.new
     responses = RSpec::OpenAPI::HashHelper::matched_paths(base, 'paths.*.*.responses.*.content.application/json')
     (request_bodies + responses).select do |paths|
       dig_schema(base, paths)&.dig('$ref')&.start_with?('#/components/schemas/')
+    end
+  end
+
+  def filter_non_generated_refs(nested_refs, base, generated_names)
+    # Reject already-generated schemas to reduce unnecessary loop
+    nested_refs.reject do |paths|
+      ref_link = base.dig(*paths)
+      schema_name = ref_link.gsub('#/components/schemas/', '')
+      generated_names.include?(schema_name)
     end
   end
 end
