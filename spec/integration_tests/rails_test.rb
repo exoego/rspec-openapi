ee7ccf4246867daf686268912a50dad60237d4ec
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2023-04-13 17:29:25 +0900

    Fix Layout/LineLength

--- spec/integration_tests/rails_test.rb
+++ spec/integration_tests/rails_test.rb
@@ -32,7 +32,8 @@ class TablesIndexTest < ActionDispatch::IntegrationTest
   # Patch minitest's ordering of examples to match RSpec's
   # in order to get comparable results
   def self.runnable_methods
-    %w[test_with_flat_query_parameters test_with_deep_query_parameters test_with_different_deep_query_parameters test_has_a_request_spec_which_does_not_make_any_request test_does_not_return_tables_if_unauthorized]
+    %w[test_with_flat_query_parameters test_with_deep_query_parameters test_with_different_deep_query_parameters
+       test_has_a_request_spec_which_does_not_make_any_request test_does_not_return_tables_if_unauthorized]
   end
 
   def test_with_flat_query_parameters
