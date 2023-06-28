496f04bd856c465f9634201aeb5e03966aad5446
exoego <ytatsuno.jp@gmail.com>
2023-06-28 11:41:13 +0900

    Load SimpleCov before rspec to collect coverage correctly

--- .simplecov_spawn.rb
+++ .simplecov_spawn.rb
@@ -6,6 +6,10 @@ unless ENV['COVERAGE'] && ENV['COVERAGE'].empty?
 
   SimpleCov.command_name 'spawn'
   SimpleCov.at_fork.call(Process.pid)
-  SimpleCov.formatter SimpleCov::Formatter::CoberturaFormatter
-  SimpleCov.start
+  SimpleCov.formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::CoberturaFormatter,
+  ])
+  SimpleCov.start do |pid|
+    add_filter "/spec/"
+  end
 end
