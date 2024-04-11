1f8270b5a82dcf4c512acf5fee72bb9bf543bdd0
Aleksei <9ceb2990-3744-4629-82f3-19bc5c80b3a2@mackevich.addymail.com>
2024-04-11 17:47:49 +0500

    Apply rubocop offences and remove bundler usage in runtime

--- lib/rspec/openapi/shared_hooks.rb
+++ lib/rspec/openapi/shared_hooks.rb
@@ -1,12 +1,8 @@
 module SharedHooks
   def self.find_extractor
-    names = Bundler.load.specs.map(&:name)
-
-    if names.include?('rails') && defined?(Rails) &&
-      Rails.respond_to?(:application) && Rails.application
+    if defined?(Rails) && Rails.respond_to?(:application) && Rails.application
       RSpec::OpenAPI::Extractors::Rails
-    elsif names.include?('hanami') && defined?(Hanami) &&
-      Hanami.respond_to?(:app) && Hanami.app?
+    elsif defined?(Hanami) && Hanami.respond_to?(:app) && Hanami.app?
       RSpec::OpenAPI::Extractors::Hanami
       # elsif defined?(Roda)
       #   some Roda extractor
