08c9fe1d573972e658dda7a8618b6e506bf264c5
Alexey Matskevich <github_job@mackevich.addymail.com>
2025-12-24 05:42:36 +0500

    test: add coverage for example_mode error messages with context
    Cover the example_mode_error method when an RSpec example is provided,
    which includes the example's full_description in the error message.

--- spec/rspec/shared_extractor_spec.rb
+++ spec/rspec/shared_extractor_spec.rb
@@ -31,5 +31,19 @@ RSpec.describe SharedExtractor do
         described_class.normalize_example_mode(123)
       end.to raise_error(ArgumentError, /example_mode must be one of/)
     end
+
+    it 'includes example context in error message when example is provided' do
+      example = double('RSpec::Core::Example', full_description: 'GET /users returns list')
+      expect do
+        described_class.normalize_example_mode(:invalid, example)
+      end.to raise_error(ArgumentError, /\(example: GET \/users returns list\)/)
+    end
+
+    it 'raises for invalid types with example context' do
+      example = double('RSpec::Core::Example', full_description: 'POST /items creates item')
+      expect do
+        described_class.normalize_example_mode([1, 2, 3], example)
+      end.to raise_error(ArgumentError, /\(example: POST \/items creates item\)/)
+    end
   end
 end
